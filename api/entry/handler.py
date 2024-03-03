554938e902e011eda3b977106a99e824c2907c45
Mike Nguyen <mike.nguyen0105@gmail.com>
2024-03-03 15:54:39 +0800

    first commit

--- api/entry/handler.py
+++ api/entry/handler.py
@@ -1,9 +1,11 @@
 import logging
+import json
 
 from openai import OpenAI
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from langchain_core.documents import Document
 from helpers.PIIManager.PIIMasking import PIIMaskingManager
+
 
 from helpers.StoreManager import store_manager
 from api.entry.models import MODEL, SYSTEM_MESSAGE_PREFIX, CreateEntry
@@ -16,8 +18,16 @@ logger = logging.getLogger(__name__)
 
 
 @router.post("/create")
-async def create_entry(email_details: CreateEntry):
-    email_details_dict = email_details.model_dump()
+async def create_entry(request: Request):
+    body_bytes = await request.body()
+    body_bytes=body_bytes.replace(b"\r", b"")
+    body_bytes=body_bytes.replace(b"\n", b"")
+    body_bytes=body_bytes.replace(b"\t", b"")
+    print(body_bytes)
+    body_bytes_str = body_bytes.decode("utf-8")
+    print(body_bytes_str[697-720])
+    email_details_dict = json.loads(body_bytes_str)
+    email_details = CreateEntry(**email_details_dict)
     logger.info(f"Create entry called\n{email_details_dict}")
     answer, document = get_response_from_ai(email_details=email_details)
     document_dict = document.dict()
@@ -35,6 +45,7 @@ async def create_entry(email_details: CreateEntry):
 def get_response_from_ai(email_details: CreateEntry) -> tuple[str, Document]:
     relevant_documents = store_manager.chroma_database.query(email_details.description)
     first_relevant_document = relevant_documents[0]
+    print(email_details.description)
     logger.info(f"All relevant documents\n{first_relevant_document}")
 
     system_message = SYSTEM_MESSAGE_PREFIX + first_relevant_document.page_content
@@ -46,13 +57,14 @@ def get_response_from_ai(email_details: CreateEntry) -> tuple[str, Document]:
     response = client.chat.completions.create(
         messages=[
             {"role": "system", "content": system_message},
-            {"role": "user", "content": question},
+            {"role": "user", "content": question, },
         ],
         model=MODEL,
     )
     masked_answer = response.choices[0].message.content
     answer = pii_manager.get_deanonymized_text(masked_answer, deanonymizer_mapping)
     logger.info(f"Answer from {MODEL}:\n{answer}")
+    answer = answer.replace("\n", "<br>")
     return answer, first_relevant_document
 
 
